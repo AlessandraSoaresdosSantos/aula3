c74416cbe3aa2c87f260f5753b8e27294b317edf
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2021-10-28 20:00:02 -0300

    implementação

--- scripts/operacao-matematica.js
+++ scripts/operacao-matematica.js
@@ -2,3 +2,13 @@ function somar(a,b)
 {
     return a + b;
 }
+
+function somarArray(elementosArray)
+{
+    let total = 0;
+
+    elementosArray.forEach(element => {
+        total += element;
+    });
+    return total;
+}
