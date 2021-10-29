405c20695fcca9e3af882d4678f34687c862064e
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2021-10-28 21:18:44 -0300

    correção

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,6 @@ function executar()
   let primeiroValor = prompt('Digite o primeiro número')
   let segundoValor = prompt('Digite o segundo número')
  
-  resultado.innerHTML =  somar(primeiroValor,segundoValor);
+  resultado.innerHTML =  somar(Number(primeiroValor),Number(segundoValor));
   resultado.style.backgroundColor = "yellow";
 }
